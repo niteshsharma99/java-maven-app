58b10ca76c072e38fc6c30e5b9df3025ed103efb
niteshsharma99 <ns476280@gmail.com>
2021-02-14 19:09:06 +0530

    project added

--- src/test/java/com/hello/HelloTest.java
+++ src/test/java/com/hello/HelloTest.java
@@ -5,19 +5,34 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class HelloTest
+
+
 	 extends TestCase
 	    
 	 {
+	
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
 	     
 	public HelloTest ( String testName )
 	     {
 	         super( testName );
 	     }
+	
+	 /**
+     * @return the suite of tests being tested
+     */
 
 	public static Test suite()
 	     {
 	         return new TestSuite( HelloTest.class );
 	     }
+	 /**
+     * Rigourous Test :-)
+     */
 
     public void testApp()
 	     {
